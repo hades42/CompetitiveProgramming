7439798bca02c4a5c630634c9281fe73f594c3d7
Van Nguyen Nguyen <nguyenvannguyen.oc@gmail.com>
2021-12-01 17:10:51 +1100

    Move to new folder

--- USACO/Brozen/CompleteSearchWithRecursion/WormHoles.cpp
+++ USACO/Brozen/CompleteSearchWithRecursion/WormHoles.cpp
@@ -31,27 +31,71 @@ void setIO(string s) { // the argument is the filename without the extension
   freopen((s + ".in").c_str(), "r", stdin);
   freopen((s + ".out").c_str(), "w", stdout);
 }
+vector<ll> X(13);
+vector<ll> Y(13);
+vector<ll> rightSide(13);
+vector<ll> pairing(13);
+ll n;
+ll res = 0;
+
+ll isCycle() {
+  for (ll i = 1; i <= n; i++) {
+    ll start = i;
+    for (ll count = 0; count < n; count++) {
+      start = pairing[start];
+      start = rightSide[start];
+    }
+    if (start) {
+      return 1;
+    }
+  }
+  return 0;
+}
+
+void solve() {
+  ll i;
+
+  // There are some wormholes havenot get paired yet
+  for (i = 1; i <= n; i++) {
+    if (pairing[i] == 0) {
+      break;
+    }
+  }
+
+  if (i == n + 1) {
+    res += isCycle();
+    return;
+  }
+  // try to pair
+  for (ll j = i + 1; j <= n; j++) {
+    if (pairing[j] == 0) {
+      pairing[i] = j;
+      pairing[j] = i;
+      solve();
+      pairing[j] = 0;
+      pairing[i] = 0;
+    }
+  }
+}
 
 int main() {
-  // cin.tie(0)->sync_with_stdio(0);
-  // cin.exceptions(cin.failbit);
-  // ll n;
-  // cin >> n;
-  // vector<tp> arr(n);
-  // for (ll i = 0; i < n; i++) {
-  // ll x, y;
-  // arr[i] = make_tuple(x, y, i);
-  //}
-  // sort(arr.begin(), arr.end());
-  // set<vector<pr>> st;
-  // do {
-  // bool flag = true;
-  // vector<pr> vt;
-  // for (ll i = 0; i < n; i += 2) {
-  // if (get<0>(arr[i]) < get<>) {
-  // vt.push_back(i, i +)
-  //}
-  //}
+  cin.tie(0)->sync_with_stdio(0);
+  cin.exceptions(cin.failbit);
+  setIO("wormhole");
+  cin >> n;
+  for (ll i = 1; i <= n; i++) {
+    cin >> X[i] >> Y[i];
+  }
 
-  //} while (next_permutation(arr.begin(), arr.end()));
+  for (ll i = 1; i <= n; i++) {
+    for (ll j = 1; j <= n; j++) {
+      if (Y[i] == Y[j] && X[i] < X[j]) {
+        if (rightSide[i] == 0 || X[j] - X[i] < X[rightSide[i]] - X[i]) {
+          rightSide[i] = j;
+        }
+      }
+    }
+  }
+  solve();
+  cout << res << endl;
 }
